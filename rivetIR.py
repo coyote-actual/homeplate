5926e1b38dee64cb4e1278cdb6c42e0fa316da82
Joseph Pistono <DarkStar@DarkStar.fios-router.home>
2017-03-05 23:01:47 -0500

    updated 'TV' commands

--- rivetIR.py
+++ rivetIR.py
@@ -11,7 +11,7 @@ def VERIZON(command):
 
 	else:
 		try:
-			def vzCipher(command):
+			def vzCmdCipher(command):
 				cmdDict = {
 					"menu":"MENU",
 					"guide":"GUIDE",
@@ -20,42 +20,37 @@ def VERIZON(command):
 					"right arrow":"CIRCLE_RIGHT",
 					"down arrow":"CIRCLE_DOWN",
 					"left arrow":"CIRCLE_LEFT",
-					"ok button":"OK_BTN"
+					"ok button":"OK_BTN",
+					"exit":"EXIT",
+					"widgets":"WIDGETS",
+					"on demand":"ON_DEMAND",
+					"options":"OPTIONS",
+					"mute":"MUTE",
+					"last":"LAST",
+					"volume up":"VOLUME_UP",
+					"volume down":"VOLUME_DOWN",
+					"fios button":"FIOS_TV_BTN",
+					"channel up":"CHANNEL_UP",
+					"channel down":"CHANNEL_DOWN",
+					"previous":"PREVIOUS",
+					"dvr":"DVR_BTN",
+					"next":"NEXT",
+					"rewind":"REWIND",
+					"play":"PLAY_BTN",
+					"pause":"PAUSE_BTN",
+					"fast forward":"FFWD",
+					"stop":"STOP",
+					"record":"REC"
 				}
 
-				return cmdDict[command.lower()]
-				'''
-				return {
-					"menu":"MENU",
-					"guide":"GUIDE",
-					"info":"INFO",
-					"up arrow":"CIRCLE_UP",
-					"right arrow":"CIRCLE_RIGHT",
-					"down arrow":"CIRCLE_DOWN",
-					"left arrow":"CIRCLE_LEFT",
-					"ok button":"OK_BTN"
-				}[command]
-				'''
+				return cmdDict[command.lower()] #this approach makes sure all commands are lower case!
 
-			print(vzCipher(command))
+
+			print(vzCmdCipher(command))
 
 		except:
 			print("Command not found!")
 
-
-
-
-
-	'''
-	if command == 'on':
-		irsend.send_once('VERIZON_STB', ['TV_POWER'])
-		irsend.send_once('VERIZON_STB', ['STB_POWER'])
-
-
-	elif command == 'off':
-		irsend.send_once('VERIZON_STB', ['TV_POWER'])
-		irsend.send_once('VERIZON_STB', ['STB_POWER'])
-	'''
 		
 
 
